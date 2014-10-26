a04a0289ac44440a283aff85db787d8f12193485
Chun Kiet <kiet5629@hotmail.com>
2014-10-26 15:06:32 -0700

    classParser updated, succcessfully read all important information from classycle's Analysed XML file

--- classParser/src/classParser/ReadXMLFile.java
+++ classParser/src/classParser/ReadXMLFile.java
@@ -22,28 +22,40 @@ public class ReadXMLFile {
 	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
 	doc.getDocumentElement().normalize();
  
-	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+	//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
  
 	NodeList nList = doc.getElementsByTagName("classes");
-	//nList = doc.getElementsByTagName("classes");
-	System.out.println("----------------------------");
+	//Node nNode = nList.item(1);
+	
+	//System.out.println("----------------------------");
  
 	for (int temp = 1; temp < nList.getLength(); temp++) {
  
 		Node nNode = nList.item(temp);
- 
-		System.out.println("\nCurrent Element :" + nNode.getNodeName());
- 
+		//System.out.println("\nCurrent Element :" + nNode.getNodeName());
 		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
- 
 			Element eElement = (Element) nNode;
+			//System.out.println("numberOfExternalClasses : " + eElement.getAttribute("numberOfExternalClasses"));
 			
-			System.out.println("numberOfExternalClasses : " + eElement.getAttribute("numberOfExternalClasses"));
-			//System.out.println("First Name : " + eElement.getElementsByTagName("classRef").item(0).getAttributes());
-			System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-			System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
- 
+			NodeList mList = eElement.getElementsByTagName("class");
+				for(int temp1 = 0; temp1 < mList.getLength(); temp1++){
+					Node mNode = mList.item(temp1);
+					//System.out.println("\nCurrent Element :" + mNode.getNodeName());
+					if (mNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement1 = (Element) mNode;
+						System.out.println(temp1 + " Class Name : " + eElement1.getAttribute("name"));
+						
+						NodeList oList = eElement1.getElementsByTagName("classRef");
+						for(int temp2 = 0; temp2 < oList.getLength(); temp2++){
+							Node oNode = oList.item(temp2);
+							if (oNode.getNodeType() == Node.ELEMENT_NODE) {
+								Element eElement2 = (Element) oNode;
+								System.out.println("Link to : " + eElement2.getAttribute("name"));
+							}
+						}
+					}
+					System.out.println("");
+				}
 		}
 	}
     } catch (Exception e) {
